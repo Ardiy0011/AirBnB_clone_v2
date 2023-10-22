f01af719bb8c91fb9fb159d6a7124cea07291876
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-21 21:19:17 -0700

    relaod function

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -22,7 +23,7 @@ class State(BaseModel, Base):
         """ Returns a list of city objects """
         city_list = []
         from models import storage
-        for city in storage.all("City").values():
+        for city in storage.all(City).values():
             if city.state_id == self.id:
                 city_list.append(city)
         return city_list
