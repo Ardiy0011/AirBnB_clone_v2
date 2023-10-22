71c9ec062a663f12d2ba1504d5acaa88325711b4
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-21 21:56:58 -0700

    relaod function

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -22,18 +22,13 @@ def close_session(exception):
 """run the Flask app """
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """A route to /states_list"""
-    data = sorted(storage.all(State).values(), key=lambda state: state.name)
-    return render_template("7-states_list.html", states=data)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('cities_by_states.html', states=sorted_states)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def citiesInStates():
-    """A route to cities in states"""
-    data = sorted(storage.all(City.state_id).values())
-    return render_template("8-cities_by_states.html", cities=data)
 """run the Flask app """
 
 
