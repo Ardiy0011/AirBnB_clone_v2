95f71beb7b9f2cb8386cecf0be75ca510d76f144
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-21 22:01:25 -0700

    relaod function

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,36 +1,20 @@
-#!/usr/bin/python3
-"""Create a Flask application"""
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.state import State
-from operator import attrgetter
 from models.city import City
 
-app = Flask(__name__)
-
-
-"""run the Flask app """
-
+app = Flask(__name)
+app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
-def close_session(exception):
-    """Closes SQLAlchemy session after each request."""
+def teardown_session(exception):
     storage.close()
 
-
-"""run the Flask app """
-
-
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route('/cities_by_states')
 def cities_by_states():
     states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda state: state.name)
     return render_template('cities_by_states.html', states=sorted_states)
-
-
-"""run the Flask app """
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
