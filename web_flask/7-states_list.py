58d348f33ed456a707cd4bab8babdbc8ad5b98b8
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-21 21:07:51 -0700

    time

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
-""" A route to /states_list """
-from flask import Flask, render_template
+"""Create a Flask application"""
+from flask import Flask, render_template, jsonify
 from models import storage
 from models.state import State
+from operator import attrgetter
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    """retireve all objects from db"""
+    data = storage.all()
+    return jsonify(data)
+
+
+"""run the Flask app """
 
 
 @app.teardown_appcontext
-def close_db(exception):
+def close_session(exception):
+    """Closes SQLAlchemy session after each request."""
     storage.close()
 
 
-@app.route('/states_list')
+"""run the Flask app """
+
+
+@app.route("/states_list" , strict_slashes=False)
 def states_list():
-    """retrieve all states in order"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-
-    return render_template('7-states_list.html',
-                           sorted_states=sorted_states)
+    """A route to /states_list"""
+    data = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template("7-states_list.html", states=data)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+"""run the Flask app """
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
