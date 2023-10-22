32dc7bc33d0d970081b76980be5d56b9d54c105b
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-21 23:04:42 -0700

    relaod function

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,12 +3,13 @@
 from models import storage
 from flask import Flask
 from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
@@ -16,7 +17,7 @@ def teardown(exc):
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """Displays related cities"""
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,6 +3,7 @@
 from models import storage
 from flask import Flask
 from flask import render_template
+from models.state import State
 
 
 app = Flask(__name__)
@@ -24,7 +25,7 @@ def states():
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
+    for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
