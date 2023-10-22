e0dedaa24c804c602983e5a9a0c18de949105c04
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-21 21:16:06 -0700

    relaod function

--- models/state.py
+++ models/state.py
@@ -9,10 +9,11 @@ import os
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    
+
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
         name = ""
 
